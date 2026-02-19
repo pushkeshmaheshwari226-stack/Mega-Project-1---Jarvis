6b1e9a44a4d18395bbc0951d52942d882c65c9ea
Pushkesh Maheshwar <pushkeshmaheshwari226@gmail.com>
2026-02-19 20:27:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,105 @@
+import webbrowser
+import speech_recognition as sr
+import pyttsx3
+import musiclib
+import feedparser
+from openai import OpenAI
+
+# object creation
+recognizer=sr.Recognizer()
+engine=pyttsx3.init() 
+new_api= "f86250efecbc4ef49a0384b0948d607b"
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key="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"
+)
+
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def read_google_news():
+    feed = feedparser.parse("https://news.google.com/rss?hl=en-IN&gl=IN&ceid=IN:en")
+    for entry in feed.entries[:5]:
+        engine.say(str(entry.title))
+
+def aiProcess(user_input):
+    try:
+        completion = client.chat.completions.create(
+            model="google/gemma-3n-e2b-it:free",
+
+            messages=[{"role": "system", "content": "You are a virtual assistant named jarvis skilled in general tasks like Alexa and Google Cloud."
+            " Give short responses please"},
+            {"role": "user", "content": user_input}
+
+    ]
+    )
+
+        reply = completion.choices[0].message.content
+        print("Jarvis:", reply)
+        engine.say(reply)
+        
+    except Exception as e:
+        print("Error talking to AI:", e)
+        engine.say("Sorry, I could not connect to the AI.")
+
+           
+
+def processCommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://facebook.com")
+
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+
+    elif "open linkedin" in c.lower():
+        webbrowser.open("https://linkedin.com")
+
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        link = musiclib.music[song]
+        webbrowser.open(link)
+    
+    elif "news today" in c.lower():
+        read_google_news()
+
+    else:
+        # Let OpenAI handle the request
+        output = aiProcess(c)
+        speak(output) 
+
+if __name__=="__main__":
+    speak("Hello I am Jarvis your Assistant !")
+    
+    while True:
+        r= sr.Recognizer()
+
+        try:
+            # obtain audio from the microphone and recognize speech using Google 
+            with sr.Microphone() as source:
+                print("Say Jarvis to Activate:-")
+                audio = r.listen(source,timeout=1)
+
+            word=r.recognize_google(audio)
+
+            if word.lower()=="exit":
+                print("SHUTDOWN")
+                break
+
+            # Listen for the wake word "Jarvis"
+            elif word.lower()=="jarvis":
+                with sr.Microphone() as source:
+                    engine.say("Jarvis Activated")
+                    audio = r.listen(source,timeout=5)
+
+                command=r.recognize_google(audio)
+                print("You said:- ",command)
+                processCommand(command)
+
+        except Exception as e:
+            print("Error; {}".format(e))
---
+++ musiclib.py
@@ -0,0 +1,10 @@
+
+music = {
+    "travis": "https://youtu.be/TE3xacTtjxw",
+    "don": "https://youtu.be/q9mXPy02Vsc",
+    "weekend": "https://youtu.be/OZCJ2DxaIJw",
+    "drake": "https://youtu.be/RrEKe4jc6Vg",
+    "dave": "https://youtu.be/u6K3hdr7jNk",
+    "kendrick": "https://youtu.be/m5edw2U_24Y",
+    "central": "https://youtu.be/cv-675iw_mQ"   
+}
